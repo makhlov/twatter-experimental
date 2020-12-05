77d59e5243cee2af383da05433599dd698815c1b
Makhlov <makhlov.egor@gmail.com>
2020-12-05 19:49:01 +0300

    Added Bootstrap
    Signed-off-by: Makhlov <makhlov.egor@gmail.com>

--- src/main/java/org/twatter/domain/User.java
+++ src/main/java/org/twatter/domain/User.java
@@ -12,7 +12,7 @@ import java.util.Set;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
     private String username;
     private String password;
     private boolean active;
@@ -22,11 +22,15 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    public Integer getId() {
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
